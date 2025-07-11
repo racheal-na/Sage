2dd327844cb1aaf664c1756ddee50204f1cd25f5
rachel <rahelnaba716@gmail.com>
2025-07-11 04:08:17 -0700

    maximum

---
+++ c++/project8.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int fn,sn;
+	cout<<"Enter first number"<<endl;
+	cin>>fn;
+	cout<<"Enter second number"<<endl;
+	cin>>sn;
+	if(fn>sn)
+{ 
+   cout<<fn<<endl;
+}
+   	else{
+   		cout<<sn<<endl;
+	   }
+return 0;
+	
+	
+	
+}
