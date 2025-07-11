19f500a3695c5507dabe5862a241f67583068f69
rachel <rahelnaba716@gmail.com>
2025-07-11 03:56:47 -0700

    cel_to_fehr

--- c++/project7.cpp
+++ c++/project7.cpp
@@ -2,16 +2,26 @@
 using namespace std;
 
 void celsius_to_fehrenit(double c){
+	double f;
+	f=(c*1.8)+32;
+	cout<<f<<endl;
+	
+	
+}
 
- 
-    double f; 
 
-f=(c*1.8)+32;
 
-cout<<f<<endl;}
+ void fehrenit_to_celsius(double f){
+	double c; 
+	c=(f-32)*5/9;
+	cout<<c<<endl;
+
+
+}
 
 
 int main(){
-	celsius_to_fehrenit(4);
+	celsius_to_fehrenit(32);
+	fehrenit_to_celsius(8);
 	return 0;
 }
