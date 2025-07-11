1eaef46080a3559b12c856465fada26e8fbedec3
rachel <rahelnaba716@gmail.com>
2025-07-11 03:43:12 -0700

    function 1

---
+++ c++/project7.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void celsius_to_fehrenit(double c){
+
+ 
+    double f; 
+
+f=(c*1.8)+32;
+
+cout<<f<<endl;}
+
+
+int main(){
+	celsius_to_fehrenit(4);
+	return 0;
+}
