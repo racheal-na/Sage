b3855621ab844f01ed146936c0555fe18722dd92
rachel <rahelnaba716@gmail.com>
2025-07-13 02:35:37 -0700

    num3 question

---
+++ c++/num3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int day;
+	cout<<"Enter number (1-7): "<<endl;
+	cin>>day;
+	switch(day){
+		case 1:cout<<"Sunday"<<endl;
+		break;
+		case 2:cout<<"Monday"<<endl;
+		break;
+		case 3:cout<<"Tuesday"<<endl;
+		break;
+		case 4:cout<<"Wednesday"<<endl;
+		break;
+		case 5:cout<<"Thursday"<<endl;
+		break;
+		case 6:cout<<"Friday"<<endl;
+		break;
+		case 7:cout<<"Saturday"<<endl;
+		break;
+		default:
+			cout<<"Invalid number"<<endl;
+	}
+	return 0;
+
+}
