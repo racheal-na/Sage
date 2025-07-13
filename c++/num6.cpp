e62f8f86f54672291f85e58bf0bf0ac6c8e6cbd8
rachel <rahelnaba716@gmail.com>
2025-07-13 05:22:59 -0700

    num6 question

---
+++ c++/num6.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i=i;
+
+	cout<<"Even numbers from 1 to 20: "<<endl;
+	while(i<=20)
+	{
+	if(i%2==0){
+		
+	cout<<i<<endl; 
+}
+i++;
+}
+return 0;
+}
