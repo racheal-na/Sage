e370ede823bb4070a92349038631f6c7a1205ddc
rachel <rahelnaba716@gmail.com>
2025-07-14 07:09:04 -0700

    star

---
+++ c++/cpp8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	cout<<"Enter numbers raw: "<<endl;
+	cin>>a;
+	for(int i=1;i<=a;++i){
+		for(int j=1;j<=(a-i);++j){
+			cout<<" ";
+		}
+		for(int k=1;k<=i;++k){
+			cout<<"*"<<endl;
+		}
+	}
+return 0;	
+}
