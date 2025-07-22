457eb1a758626d24198a9fbedee31f041af341f9
rachel <rahelnaba716@gmail.com>
2025-07-22 03:54:04 -0700

    index and script

--- HTML,CSS and Java script/my website/script.js
+++ HTML,CSS and Java script/my website/script.js
@@ -1,3 +1,49 @@
-<script>
+function greetuser(){
+    alert('hi javascript from script')
+}
+ function add(){
+    let x=1;
+    let y=3;
+    y=7
+    alert(x+y)
+}
+let x=2
+let y=3
+ let isJavascript= true;
+ isJavascript=1;
+ let array=["A","B","C"]
+ let person={name: "rachel", age: 23, gender: "femal"}
+ console.log("addition: ",x+y);
+ console.log("boolean: ",isJavascript);
+ console.log("miss behave: ","1"+1);
+ array[2]=50;
+ console.log("Array: ",array[2]);
+ console.log("person name: ",person.name);
+ person.name="naol"
+ console.log("person name: ",person.name);
+ let age=18;
+ if (age>=18){
+    console.log("You are adult.")
+ }else{
+    console.log("You are a minor.")
+ }
+ switch(age){
+   case 17:
+    console.log("age: ",17)
+    break;
+    case 18:
+        console.log("age: ",18)
+        break;
+  default:
+       console.log("not found")
+ }
+for(let i=0;i<=5;i++){
+    console.log(i)
+}
+let i=-5
+while(i<=5){
+    console.log(i);
+    i++;
+}
     
-</script>
+
